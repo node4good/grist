c655a89dde2df7b417f68e1cd869d5c07bf56d08
Refael Ackermann <refael@empeeric.com>
2014-01-24 21:30:28 +0200

    test are running

--- test/search-test.js
+++ test/search-test.js
@@ -13,7 +13,7 @@ var words = ["Sergey Brin", "Serg Kosting", "Pupking Sergey", "Munking Sirgey"];
 
 describe('Search', function () {
     describe('New store', function () {
-        var db, coll;
+        var db, collection;
         before(function (done) {
             tutils.getDb('test', true, safe.sure(done, function (_db) {
                 db = _db;
@@ -22,8 +22,8 @@ describe('Search', function () {
         });
         before(function (done) {
             db.collection("test1", {}, safe.sure(done, function (_coll) {
-                coll = _coll;
-                coll.ensureIndex({num: 1}, {sparse: false, unique: false}, safe.sure(done, function (name) {
+                collection = _coll;
+                collection.ensureIndex({num: 1}, {sparse: false, unique: false}, safe.sure(done, function (name) {
                     assert.ok(name);
                     done();
                 }));
@@ -45,7 +45,7 @@ describe('Search', function () {
                         delete obj.num;
                         delete obj.pum;
                     }
-                    coll.insert(obj, cb);
+                    collection.insert(obj, cb);
                     if (obj.sin > 0 && obj.sin < 0.5)
                         gt0sin++;
                     i++;
@@ -54,41 +54,41 @@ describe('Search', function () {
             );
         });
         it("Has right size", function (done) {
-            coll.count(safe.sure(done, function (count) {
+            collection.count(safe.sure(done, function (count) {
                 assert.equal(count, num);
                 done();
             }));
         });
         it("find {num:10} (index)", function (done) {
-            coll.find({num: 10}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: 10}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 1);
                 assert.equal(docs[0].num, 10);
                 done();
             }));
         });
         it("find {num:{$not:{$ne:10}}} (index)", function (done) {
-            coll.find({num: {$not: {$ne: 10}}}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: {$not: {$ne: 10}}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 1);
                 assert.equal(docs[0].num, 10);
                 done();
             }));
         });
         it("find {pum:10} (no index)", function (done) {
-            coll.find({pum: 10}).toArray(safe.sure(done, function (docs) {
+            collection.find({pum: 10}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 1);
                 assert.equal(docs[0].pum, 10);
                 done();
             }));
         });
         it("find {pum:{eq:10}} (no index)", function (done) {
-            coll.find({num: 10}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: 10}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 1);
                 assert.equal(docs[0].pum, 10);
                 done();
             }));
         });
         it("find {num:{$lt:10}} (index)", function (done) {
-            coll.find({num: {$lt: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: {$lt: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 8);
                 _.each(docs, function (doc) {
                     assert.ok(doc.num < 10);
@@ -97,7 +97,7 @@ describe('Search', function () {
             }));
         });
         it("find {pum:{$lt:10}} (no index)", function (done) {
-            coll.find({pum: {$lt: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({pum: {$lt: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 8);
                 _.each(docs, function (doc) {
                     assert.ok(doc.pum < 10);
@@ -106,7 +106,7 @@ describe('Search', function () {
             }));
         });
         it("find {num:{$lte:10}} (index)", function (done) {
-            coll.find({num: {$lte: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: {$lte: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 9);
                 _.each(docs, function (doc) {
                     assert.ok(doc.num <= 10);
@@ -115,7 +115,7 @@ describe('Search', function () {
             }));
         });
         it("find {pum:{$lte:10}} (no index)", function (done) {
-            coll.find({pum: {$lte: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({pum: {$lte: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 9);
                 _.each(docs, function (doc) {
                     assert.ok(doc.pum <= 10);
@@ -124,7 +124,7 @@ describe('Search', function () {
             }));
         });
         it("find {num:{$gt:10}} (index)", function (done) {
-            coll.find({num: {$gt: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: {$gt: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 849);
                 _.each(docs, function (doc) {
                     assert.ok(doc.num > 10);
@@ -133,7 +133,7 @@ describe('Search', function () {
             }));
         });
         it("find {pum:{$gt:10}} (no index)", function (done) {
-            coll.find({pum: {$gt: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({pum: {$gt: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 849);
                 _.each(docs, function (doc) {
                     assert.ok(doc.pum > 10);
@@ -142,7 +142,7 @@ describe('Search', function () {
             }));
         });
         it("find {num:{$gte:10}} (index)", function (done) {
-            coll.find({num: {$gte: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: {$gte: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 850);
                 _.each(docs, function (doc) {
                     assert.ok(doc.num >= 10);
@@ -151,7 +151,7 @@ describe('Search', function () {
             }));
         });
         it("find {pum:{$gte:10}} (no index)", function (done) {
-            coll.find({pum: {$gte: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({pum: {$gte: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 850);
                 _.each(docs, function (doc) {
                     assert.ok(doc.pum >= 10);
@@ -160,7 +160,7 @@ describe('Search', function () {
             }));
         });
         it("find {num:{$ne:10}} (index)", function (done) {
-            coll.find({num: {$ne: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: {$ne: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, num - 1);
                 _.each(docs, function (doc) {
                     assert.ok(doc.num != 10);
@@ -169,7 +169,7 @@ describe('Search', function () {
             }));
         });
         it("find {num:{$not:{$eq:10}}} (index)", function (done) {
-            coll.find({num: {$ne: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: {$ne: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, num - 1);
                 _.each(docs, function (doc) {
                     assert.ok(doc.num != 10);
@@ -178,7 +178,7 @@ describe('Search', function () {
             }));
         });
         it("find {pum:{$ne:10}} (no index)", function (done) {
-            coll.find({pum: {$ne: 10}}).toArray(safe.sure(done, function (docs) {
+            collection.find({pum: {$ne: 10}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, num - 1);
                 _.each(docs, function (doc) {
                     assert.ok(doc.pum != 10);
@@ -187,7 +187,7 @@ describe('Search', function () {
             }));
         });
         it("find {num:{$in:[10,20,30,40]}} (index)", function (done) {
-            coll.find({num: {$in: [10, 20, 30, 40]}}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: {$in: [10, 20, 30, 40]}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 4);
                 _.each(docs, function (doc) {
                     assert.ok(doc.num % 10 === 0);
@@ -196,7 +196,7 @@ describe('Search', function () {
             }));
         });
         it("find {pum:{$in:[10,20,30,40]}} (no index)", function (done) {
-            coll.find({pum: {$in: [10, 20, 30, 40]}}).toArray(safe.sure(done, function (docs) {
+            collection.find({pum: {$in: [10, 20, 30, 40]}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 4);
                 _.each(docs, function (doc) {
                     assert.ok(doc.num % 10 === 0);
@@ -205,7 +205,7 @@ describe('Search', function () {
             }));
         });
         it("find {num:{$nin:[10,20,30,40]}} (index)", function (done) {
-            coll.find({num: {$nin: [10, 20, 30, 40]}}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: {$nin: [10, 20, 30, 40]}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, num - 4);
                 _.each(docs, function (doc) {
                     assert.ok(doc.num != 10 && doc.num != 20 && doc.num != 30 && doc.num != 40);
@@ -214,7 +214,7 @@ describe('Search', function () {
             }));
         });
         it("find {pum:{$nin:[10,20,30,40]}} (no index)", function (done) {
-            coll.find({pum: {$nin: [10, 20, 30, 40]}}).toArray(safe.sure(done, function (docs) {
+            collection.find({pum: {$nin: [10, 20, 30, 40]}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, num - 4);
                 _.each(docs, function (doc) {
                     assert.ok(doc.pum != 10 && doc.pum != 20 && doc.pum != 30 && doc.pum != 40);
@@ -223,7 +223,7 @@ describe('Search', function () {
             }));
         });
         it("find {num:{$not:{$lt:10}}} (index)", function (done) {
-            coll.find({num: {$not: {$lt: 10}}}).toArray(safe.sure(done, function (docs) {
+            collection.find({num: {$not: {$lt: 10}}}).toArray(safe.sure(done, function (docs) {
                 assert.ok(docs.length == 992 || docs.length == 850);	// Mongo BUG, 850 is wrong
                 _.each(docs, function (doc) {
                     assert.ok(_.isUndefined(doc.num) || doc.num >= 10);
@@ -232,7 +232,7 @@ describe('Search', function () {
             }));
         });
         it("find {pum:{$not:{$lt:10}}} (no index)", function (done) {
-            coll.find({pum: {$not: {$lt: 10}}}).toArray(safe.sure(done, function (docs) {
+            collection.find({pum: {$not: {$lt: 10}}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 992);
                 _.each(docs, function (doc) {
                     assert.ok(_.isUndefined(doc.pum) || doc.pum >= 10);
@@ -241,7 +241,7 @@ describe('Search', function () {
             }));
         });
         it("find {num:{$lt:10},$or:[{num:5},{num:6},{num:11}]}", function (done) {
-            coll.find({num: {$lt: 10}, $or: [
+            collection.find({num: {$lt: 10}, $or: [
                 {num: 5},
                 {num: 6},
                 {num: 11}
@@ -254,7 +254,7 @@ describe('Search', function () {
             }));
         });
         it("find {num:{$lt:10},$nor:[{num:5},{num:6},{num:7}", function (done) {
-            coll.find({num: {$lt: 10}, $nor: [
+            collection.find({num: {$lt: 10}, $nor: [
                 {num: 5},
                 {num: 6},
                 {num: 7}
@@ -267,73 +267,73 @@ describe('Search', function () {
             }));
         });
         it("find {'anum':{$all:[1,2,3]}} (index)", function (done) {
-            coll.find({'anum': {$all: [1, 2, 3]}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'anum': {$all: [1, 2, 3]}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 1);
                 done();
             }));
         });
         it("find {'apum':{$all:[1,2,3]}} (no index)", function (done) {
-            coll.find({'apum': {$all: [1, 2, 3]}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'apum': {$all: [1, 2, 3]}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 1);
                 done();
             }));
         });
         it("find {'pum':{$exists:false}} (no index)", function (done) {
-            coll.find({'pum': {$exists: false}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'pum': {$exists: false}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 142);
                 done();
             }));
         });
         it("find {'num':{$exists:false}} (index)", function (done) {
-            coll.find({'num': {$exists: false}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'num': {$exists: false}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 142);
                 done();
             }));
         });
         it("find {'pum':{$exists:true}} (no index)", function (done) {
-            coll.find({'pum': {$exists: true}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'pum': {$exists: true}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 858);
                 done();
             }));
         });
         it("find {'num':{$exists:true}} (index)", function (done) {
-            coll.find({'num': {$exists: true}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'num': {$exists: true}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 858);
                 done();
             }));
         });
         it("find {'junk':{$regex:'Sergey'}}", function (done) {
-            coll.find({'junk': {$regex: 'Sergey'}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'junk': {$regex: 'Sergey'}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 500);
                 done();
             }));
         });
         it("find {'junk':/Sergey/i}", function (done) {
-            coll.find({'junk': /seRgey/i}).toArray(safe.sure(done, function (docs) {
+            collection.find({'junk': /seRgey/i}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 500);
                 done();
             }));
         });
         it("find {'junk':{$regex:'seRgey',$options:'i'}}", function (done) {
-            coll.find({'junk': {$regex: 'seRgey', $options: 'i'}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'junk': {$regex: 'seRgey', $options: 'i'}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 500);
                 done();
             }));
         });
         it("find {'junk':{$options:'i',$regex:'seRgey'}}", function (done) {
-            coll.find({'junk': {$options: 'i', $regex: 'seRgey'}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'junk': {$options: 'i', $regex: 'seRgey'}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 500);
                 done();
             }));
         });
         it("find {'junk':{$not:/sirgei/i}}", function (done) {
-            coll.find({'junk': {$not: /sirgey/i}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'junk': {$not: /sirgey/i}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 750);
                 done();
             }));
         });
         it("find {'words':{$all:[/sirgey/i,/sergey/i]}}", function (done) {
-            coll.find({'words': {$all: [/sirgey/i, /sergey/i]}}).toArray(safe.sure(done, function (docs) {
+            collection.find({'words': {$all: [/sirgey/i, /sergey/i]}}).toArray(safe.sure(done, function (docs) {
                 assert.equal(docs.length, 142);
                 done();
             }));
