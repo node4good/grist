c03c43b2a087517003b913af7936319426e10971
Refael Ackermann <refael@empeeric.com>
2014-01-26 11:37:28 +0200

    "basic" -> "new store" green

--- test/basic-test.js
+++ test/basic-test.js
@@ -1,3 +1,4 @@
+"use strict";
 var assert = require('assert');
 var _ = require('lodash');
 var safe = require('safe');
@@ -15,68 +16,78 @@ var _id = null;
 describe('Basic', function () {
     this.timeout(60 * 60 * 1000);
     describe('New store', function () {
-        before("open DB", function (done) {
+
+        before(function (done) {
+            var test = this;
             tutils.getDb('test', true, function (err, _db) {
-                this.db = _db;
-                this.db.collection("test", {}, function (err, _coll) {
-                    this.coll = _coll;
-                    done();
+                test._db = _db;
+                test._db.collection("test", {}, function (err, _coll) {
+                    test.coll = _coll;
+                    var p = new Promise;
+                    p.fulfill();
+                    gt0sin = 0;
+                    _dt = null;
+                    _.times(num, function (i) {
+                        var d;
+                        if (!_dt) _dt = d = new Date();
+                        else d = new Date(_dt.getTime() + 1000 * i);
+                        var obj = {
+                            _dt: d,
+                            dum: parseInt(i / 2),
+                            num: i,
+                            pum: i,
+                            sub: {num: i},
+                            sin: Math.sin(i),
+                            cos: Math.cos(i),
+                            t: 15,
+                            junk: loremIpsum({count: 1, units: "paragraphs"})
+                        };
+                        obj.txt = obj.sin > 0 && "greater than zero" || obj.sin < 0 && "less than zero" || "zero";
+                        if (obj.sin > 0 && obj.sin < 0.5)
+                            gt0sin++;
+                        p = p.then(function () {
+                            return test.coll.insert(obj);
+                        });
+                    });
+                    p.then(function () {
+                        done();
+                    }).end();
                 });
             });
         });
-        it("Populated with test data", function (done) {
-            var p = new Promise;
-            p.fulfill();
-            gt0sin = 0;
-            _dt = null;
-            _.times(num, function (i) {
-                var d;
-                if (!_dt) _dt = d = new Date();
-                else d = new Date(_dt.getTime() + 1000 * i);
-                var obj = {_dt: d, dum: parseInt(i / 2), num: i, pum: i, sub: {num: i}, sin: Math.sin(i), cos: Math.cos(i), t: 15, junk: loremIpsum({count: 1, units: "paragraphs"})};
-                obj.txt = obj.sin > 0 && "больше нуля" || obj.sin < 0 && "меньше нуля" || "ноль";
-                if (obj.sin > 0 && obj.sin < 0.5)
-                    gt0sin++;
-                p = p.then(function () {
-                    return this.coll.insert(obj);
-                });
-            });
-            p.then(function () { done(); }).end();
-        });
+
         it("Has right size", function (done) {
             this.coll.count(function (err, count) {
                 assert.equal(count, num);
                 done(err);
             });
         });
+
         after(function (done) {
-            this.db.close(done);
-            delete this.colll;
-            delete this.db;
+            this._db.close(done);
+            delete this.coll;
+            delete this._db;
         });
+
     });
+
+
     describe('Existing store', function () {
         var db, coll;
         before(function (done) {
-            tutils.getDb('test', false, safe.sure(done, function (_db) {
+            tutils.getDb('test', false, function (err, _db) {
                 db = _db;
-                db.collection("test", {}, safe.sure(done, function (_coll) {
+                db.collection("test", {}, function (err, _coll) {
                     coll = _coll;
-                    coll.ensureIndex({sin: 1}, safe.sure(done, function () {
-                        coll.ensureIndex({num: 1}, safe.sure(done, function () {
-                            coll.ensureIndex({_dt: 1}, safe.sure(done, function () {
-                                done();
-                            }));
-                        }));
-                    }));
-                }));
-            }));
+                    done();
+                });
+            });
         });
-        it("Has right size", function (done) {
-            coll.count(safe.sure(done, function (count) {
+        it("Collection.count", function (done) {
+            coll.count(function (err, count) {
                 assert.equal(count, num);
-                done();
-            }));
+                done(err);
+            });
         });
         it("utf8 text", function (done) {
             coll.find({ sin: { $gt: 0 } }).toArray(safe.sure(done, function (docs) {
@@ -186,16 +197,19 @@ describe('Basic', function () {
                 done();
             }));
         });
+
         it("find with fields {'sub.num':1}", function (done) {
-            coll.find({num: 10}, {'sub.num': 1}).toArray(safe.sure(done, function (docs) {
+            coll.find({num: 10}, {'sub.num': 1}).toArray(function (err, docs) {
                 assert.equal(_.size(docs[0]), 2);
                 assert.equal(docs[0].sub.num, 10);
-                done();
-            }));
+                done(err);
+            });
         });
+
         it("dummy update", function (done) {
-            coll.update({pum: 11}, {$set: {num: 10, "sub.tub": 10, "sub.num": 10}, $unset: {sin: 1}}, safe.sure(done, function () {
-                coll.find({pum: 11}).toArray(safe.sure(done, function (docs) {
+            coll.update({pum: 11}, {$set: {num: 10, "sub.tub": 10, "sub.num": 10}, $unset: {sin: 1}}, function (err) {
+                if (err) throw err;
+                coll.find({pum: 11}).toArray(function (err, docs) {
                     assert.equal(docs.length, 1);
                     var obj = docs[0];
                     assert.equal(obj.pum, 11);
@@ -203,38 +217,45 @@ describe('Basic', function () {
                     assert.equal(obj.sub.num, 10);
                     assert.equal(obj.sub.tub, 10);
                     assert.equal(obj.sin, null);
-                    done();
-                }));
-            }));
+                    done(err);
+                });
+            });
         });
+
         it("$unset and $inc on subfields", function (done) {
-            coll.update({pum: 11}, {$unset: {"sub.tub": 1}, $inc: {"sub.num": 5, "sub.pum": 3}}, safe.sure(done, function () {
-                coll.find({pum: 11}).toArray(safe.sure(done, function (docs) {
+            coll.update({pum: 11}, {$unset: {"sub.tub": 1}, $inc: {"sub.num": 5, "sub.pum": 3}}, function (err) {
+                if (err) throw err;
+                coll.find({pum: 11}).toArray(function (err, docs) {
                     assert.equal(docs.length, 1);
                     var obj = docs[0];
                     assert.equal(obj.pum, 11);
                     assert.equal(obj.sub.tub, null);
                     assert.equal(obj.sub.num, 15);
                     assert.equal(obj.sub.pum, 3);
-                    done();
-                }));
-            }));
+                    done(err);
+                });
+            });
         });
+
         it("multi update", function (done) {
-            coll.update({dum: 1}, {$set: {pum: 10}}, {multi: true}, safe.sure(done, function () {
-                coll.find({dum: 1}).sort({num: 1}).toArray(safe.sure(done, function (docs) {
+            coll.update({dum: 1}, {$set: {pum: 10}}, {multi: true}, function (err) {
+                if (err) throw err;
+                coll.find({dum: 1}).sort({num: 1}).toArray(function (err, docs) {
                     assert.equal(docs.length, 2);
                     assert.equal(docs[0].num, 2);
                     assert.equal(docs[1].num, 3);
-                    done();
-                }));
-            }));
+                    done(err);
+                });
+            });
         });
+
         it("dummy remove", function (done) {
-            coll.remove({pum: 20}, safe.sure(done, function () {
+            coll.remove({pum: 20}, function (err) {
+                if (err) throw err;
                 coll.findOne({pum: 20}, done);
-            }));
+            });
         });
+
         after(function (done) {
             db.close(done);
         });
