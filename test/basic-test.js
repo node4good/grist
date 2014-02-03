768a2c531e86b9f341fb06bdca641f1f99162046
Refael Ackermann <refael@empeeric.com>
2014-02-03 20:47:52 +0200

    Tighten up test-suite

--- test/basic-test.js
+++ test/basic-test.js
@@ -200,7 +200,7 @@ describe('Basic', function () {
             }));
         });
 
-        it.skip("find with fields {'sub.num':1}", function (done) {
+        it("find with fields {'sub.num':1}", function (done) {
             coll.find({num: 10}, {'sub.num': 1}).toArray(function (err, docs) {
                 if (err) throw err;
                 var doc = docs[0];
@@ -262,6 +262,157 @@ describe('Basic', function () {
 
         after(function (done) {
             db.close(done);
+        });
+    });
+});
+
+
+describe('C.R.U.D.', function () {
+    var db, coll;
+
+    before(function (done) {
+        tutils.getDb('test', true, function (err, _db) {
+            db = _db;
+            db.collection("test", {}, function (err, _coll) {
+                coll = _coll;
+                coll.ensureIndex({i: 1}, function () {
+                    done();
+                });
+            });
+        });
+    });
+
+    describe('Should save', function () {
+        var obj;
+        it('create new', function (done) {
+            obj = {i: 1, j: 1};
+            coll.save(obj, done);
+        });
+        it('id is assigned', function () {
+            assert(obj._id);
+        });
+        it('modify it', function (done) {
+            obj.i++;
+            coll.save(obj, safe.sure(done, function () {
+                coll.findOne({_id: obj._id}, safe.sure(done, function (obj1) {
+                    assert.deepEqual(obj, obj1);
+                    done();
+                }));
+            }));
+        });
+        it('delete it', function (done) {
+            coll.remove({_id: obj._id}, safe.sure(done, function () {
+                coll.findOne({_id: obj._id}, {sort: {i: 1}}, safe.sure(done, function (obj1) {
+                    assert(!obj1);
+                    done();
+                }));
+            }));
+        });
+    });
+
+    describe('Should update', function () {
+        var obj;
+        it('create with upsert and $set apply $set to query', function (done) {
+            obj = {j: 3, c: "multi", a: [1, 2, 3, 4, 5]};
+            coll.update({i: 2}, {$set: obj}, {upsert: true}, safe.sure(done, function (n, r) {
+                assert.equal(n, 1);
+                assert.equal(r.updatedExisting, false);
+                assert(r.upserted);
+                coll.findOne({i: 2}, safe.sure(done, function (obj1) {
+                    assert.equal(obj1.i, 2);
+                    done();
+                }));
+            }));
+        });
+        it('update array field is possible', function (done) {
+            coll.update({i: 2}, {$set: {a: [1, 2]}}, safe.sure(done, function (n, r) {
+                assert.equal(n, 1);
+                assert.equal(r.updatedExisting, true);
+                coll.findOne({i: 2}, safe.sure(done, function (obj1) {
+                    assert.deepEqual([1, 2], obj1.a);
+                    done();
+                }));
+            }));
+        });
+        it('upsert one more did not touch initial object', function (done) {
+            obj = {j: 4, i: 3, c: "multi", a: [1, 2, 3, 4, 5]};
+            var clone = _.cloneDeep(obj);
+            coll.update({i: 3}, obj, {upsert: true}, safe.sure(done, function (n, r) {
+                assert.equal(n, 1);
+                assert.equal(r.updatedExisting, false);
+                assert(r.upserted);
+                coll.findOne({i: 3}, safe.sure(done, function (obj1) {
+                    assert.deepEqual(obj, clone);
+                    clone._id = obj1._id;
+                    assert.deepEqual(obj1, clone);
+                    done();
+                }));
+            }));
+        });
+        it('modify multi changes only specific field for many documents', function (done) {
+            coll.update({c: "multi"}, {$set: {a: []}}, {multi: true}, safe.sure(done, function (n, r) {
+                assert.equal(n, 2);
+                assert.equal(r.updatedExisting, true);
+                coll.find({c: "multi"}).toArray(safe.sure(done, function (docs) {
+                    assert(docs[0].j, docs[1].j);
+                    _.each(docs, function (doc) {
+                        assert.deepEqual(doc.a, []);
+                    });
+                    done();
+                }));
+            }));
+        });
+        it.skip('update with setting of _id field is not possible', function (done) {
+            coll.update({c: "multi"}, {$set: {_id: "newId"}}, {multi: true}, function (err) {
+                assert(err);
+                done();
+            });
+        });
+    });
+
+    describe("should insert", function () {
+        it("works with String id", function (done) {
+            coll.insert({_id: "some@email.goes.here.com", data: "some data"}, safe.sure(done, function () {
+                coll.findOne({_id: "some@email.goes.here.com"}, safe.sure(done, function (obj) {
+                    assert(obj);
+                    done();
+                }));
+            }));
+        });
+        it("works with Date id", function (done) {
+            var _id = new Date();
+            coll.insert({_id: _id, data: "some data"}, safe.sure(done, function () {
+                coll.findOne({_id: _id}, safe.sure(done, function (obj) {
+                    assert(obj);
+                    done();
+                }));
+            }));
+        });
+        it("works with Number id", function (done) {
+            var _id = 1976;
+            coll.insert({_id: _id, data: "some data"}, safe.sure(done, function () {
+                coll.findOne({_id: _id}, safe.sure(done, function (obj) {
+                    assert(obj);
+                    done();
+                }));
+            }));
+        });
+    });
+
+    describe("Distinct", function () {
+        before(function (done) {
+            coll.insert([
+                { name: 'exec', age: 1 },
+                { name: 'exec', age: 2 }
+            ], done);
+        });
+
+        it("works", function (done) {
+            coll.distinct('age', {name: 'exec'}, function (err, docs) {
+                if (err) throw err;
+                assert(2, docs.length);
+                done();
+            });
         });
     });
 });
--- test/misc-test.js
+++ test/misc-test.js
@@ -5,7 +5,7 @@ var _ = require('lodash');
 var safe = require('safe');
 var tutils = require("./utils");
 
-describe.skip('Misc', function () {
+describe('Misc', function () {
     var db;
     before(function (done) {
         tutils.getDb('misc', true, safe.sure(done, function (_db) {
@@ -20,19 +20,6 @@ describe.skip('Misc', function () {
                 cursor.count(safe.sure(done, function (res) {
                     assert.equal(res, 1);
                     cursor.toArray(done);
-                }));
-            }));
-        }));
-    });
-    it('GH-19 Unset must clean key from object', function (done) {
-        db.collection("GH19", {}, safe.sure(done, function (_coll) {
-            _coll.insert({name: 'Tony', age: '37'}, safe.sure(done, function () {
-                _coll.findAndModify({}, {age: 1}, {$set: {name: 'Tony'}, $unset: { age: true }}, {'new': true}, safe.sure(done, function (doc) {
-                    assert(!_.contains(_.keys(doc), 'age'));
-                    _coll.findOne({}, {age: 1}, safe.sure(done, function (obj) {
-                        assert(!_.contains(_.keys(obj), 'age'));
-                        done();
-                    }));
                 }));
             }));
         }));
@@ -64,13 +51,6 @@ describe.skip('Misc', function () {
                         }));
                     }));
                 }));
-            }));
-        }));
-    });
-    it('GH-26 sort order can also be optional for findAndRemove', function (done) {
-        db.collection("GH26", {}, safe.sure(done, function (_coll) {
-            _coll.insert({}, safe.sure(done, function () {
-                _coll.findAndRemove({}, done);
             }));
         }));
     });
