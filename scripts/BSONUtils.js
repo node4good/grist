e4390b720a423ccfda5d21836e6533df68b704d1
Refael Ackermann <refael@empeeric.com>
2014-09-19 11:00:31 +0300

    MongoDB dump to grist

---
+++ scripts/BSONUtils.js
@@ -0,0 +1,56 @@
+'use strict';
+var _ = require('lodash-contrib');
+var fs = require('fs');
+var path = require('path');
+var Chanel = require('chanel');
+var buffalo = require('buffalo');
+var DB = require('../lib');
+var ObjectId = require('../lib/ObjectId');
+
+function bsonFileConvert(filename) {
+    var buf = fs.readFileSync(filename);
+    buf.__off = 0;
+    var objs = [];
+    while (buf.__off < buf.length) {
+        var obj = buffalo.parse(buf, buf.__off);
+        buf.__off += obj.__length;
+        if (obj._id) obj._id = ObjectId.createFromHexString(String(obj._id));
+        objs.push(obj);
+    }
+    return objs;
+}
+
+
+function bsonDirectoryConvert(direcotry, db, done) {
+    if (_.isString(db)) {
+        if (!fs.existsSync(db)) fs.mkdirSync(db);
+        db = new DB(db, {name: db});
+    }
+    var files = fs.readdirSync(direcotry).filter(
+        function (file) {
+            return file && path.extname(file) === '.bson';
+        }
+    ).map(
+        function (file) {
+            var colName = path.basename(file, '.bson');
+            var filename = path.join(direcotry, file);
+            return {col: colName, file: filename};
+        }
+    );
+    var chanel = new Chanel(1);
+    files.forEach(function forEachPair(pair) {
+        console.log(pair);
+        var objs = bsonFileConvert(pair.file);
+        var p = db.collection(pair.col).then(function (coll) {
+            return coll.drop();
+        }).then(function (coll) {
+            return coll.insert(objs);
+        }).end();
+        chanel.push(p);
+    });
+    chanel(done);
+}
+
+
+module.exports.BSONFileConvert = bsonFileConvert;
+module.exports.BSONDirectoryConvert = bsonDirectoryConvert;
--- test/bson-test.js
+++ test/bson-test.js
@@ -1,60 +1,22 @@
 "use strict";
-/*global it,describe */
-var fs = require('fs');
 var path = require('path');
-var assert = require('chai').assert;
-var buffalo = require('buffalo');
-var BSONStream = require('../scripts/BSONStream');
-
-
+var BSONUtils = require('../scripts/BSONUtils');
 var tutils = require("./utils");
 
-var gt0sin = 0;
-var _dt = null;
 
-
-describe.only('BSON Parsing', function () {
-    this.timeout(10000);
+describe('BSON Parsing', function () {
     before(function (done) {
         var test = this;
-        tutils.getDb('bson', true, function (err, _db) {
+        tutils.getDb('bson-test', true, function (err, _db) {
             test._db = _db;
-            test.Dbname = "BSON-test";
-            test._db.collection(test.Dbname, {}, function (err, _coll) {
-                test.coll = _coll;
-                test.coll.drop().then(function () {
-                    gt0sin = 0;
-                    _dt = null;
-                    done();
-                });
-            });
+            _db.dropDatabase(done);
         });
     });
 
-    var filename = path.join(__dirname, '..', 'test-data', 'bson', 'navigations.bson');
-    it("Read Stream", function (done) {
-        var str = fs.createReadStream(filename, {flags: 'r', autoClose: true});
-        var bsonStream = new BSONStream({objectMode: true});
-        str.pipe(bsonStream);
-        bsonStream.on('data', function (row) {
-            console.log(row);
-        });
-        bsonStream.on('end', function () {
-            assert.equal(this._objs.length, 35);
-            done();
-        });
-    });
 
-    it("Read", function (done) {
-        var buf = fs.readFileSync(filename);
-        buf.__off = 0;
-        var objs = [];
-        while (buf.__off < buf.length) {
-            var obj = buffalo.parse(buf, buf.__off);
-            buf.__off += obj.__length;
-            objs.push(obj);
-        }
-        console.log(objs);
-        done();
+    it("Read Dir", function (done) {
+        this.timeout(10 * 1000);
+        var fixtureDir = path.join(__dirname, '..', 'test-data', 'bson');
+        BSONUtils.BSONDirectoryConvert(fixtureDir, this._db, done);
     });
 });
